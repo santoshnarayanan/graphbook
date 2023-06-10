964076f88ce0dbfbabfa916ae49ce83fd65db40c
Santosh Narayanan <santosh33@gmail.com>
2023-06-10 19:57:42 +0200

    Express Helmet - code for XSS

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const app = express();
 
@@ -11,8 +14,21 @@ const app = express();
     res.send('Hello World!');
 }); */
 
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+    }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+
+// all files and folders are served in begining with '/'
 const root = path.join(__dirname, '../../');
 app.use('/', express.static(path.join(root, 'dist/client')));
+// uploading images
 app.use('/uploads', express.static(path.join(root, 'uploads')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(root, '/dist/client/index.html'));
