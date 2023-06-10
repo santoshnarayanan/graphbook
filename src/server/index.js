b6043a535b499246264022d1bab70735a3ccd884
Santosh Narayanan <santosh33@gmail.com>
2023-06-10 19:43:03 +0200

    Serving prod build - test build process

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.get('/', function (req, res, next) {
+/* app.get('/', function (req, res, next) {
     console.log('first function');
     next();
 }, function (req, res) {
     console.log('second function');
     res.send('Hello World!');
+}); */
+
+const root = path.join(__dirname, '../../');
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(root, '/dist/client/index.html'));
 });
 app.listen(8000, () => console.log('Listening on port 8000'));
