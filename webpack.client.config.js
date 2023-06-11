b87462f7651f7f106e6af12cbe84f40bd80c848b
Santosh Narayanan <santosh33@gmail.com>
2023-06-11 15:47:03 +0200

    PAckage upgrade

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const buildDirectory = 'dist';
 const outputDirectory = buildDirectory + '/client';
 module.exports = {
